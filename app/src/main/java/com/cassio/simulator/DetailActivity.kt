b20ddedad85334b993dd3faa8be257c854996d70
cassiobetti <cassio_betti@yahoo.com.br>
2022-06-06 11:19:58 -0300

    Desenvolvimento dos layouts de listagem e detalhes de partidas.

---
+++ app/src/main/java/com/cassio/simulator/DetailActivity.kt
@@ -0,0 +1,35 @@
+package com.cassio.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cassio.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
